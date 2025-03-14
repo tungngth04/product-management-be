9b412af159a216040d91d7a6bc0dada04174d546
tungvhn <tungviet624@gmail.com>
2025-03-14 22:18:44 +0700

    feat: creat admin dashboard

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const route = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route");
 
 const database = require("./config/database");
 
@@ -15,6 +16,7 @@ app.set("view engine", "pug");
 app.use(express.static("public"));
 
 route(app);
+routeAdmin(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
