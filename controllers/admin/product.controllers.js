4461906d813d313c9dd62894f53d891cd879ed00
tungvhn <tungviet624@gmail.com>
2025-03-15 15:54:54 +0700

    feat: get data in products page admin

--- controllers/admin/product.controllers.js
+++ controllers/admin/product.controllers.js
@@ -1,5 +1,12 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+// [GET] /admin/products
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false,
+  });
+  console.log(products);
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
+    products: products,
   });
 };
--- controllers/client/home.controllers.js
+++ controllers/client/home.controllers.js
@@ -1,4 +1,5 @@
 //- controller nhận request từ client và trả về response để render view
+// [GET] /
 module.exports.index = (req, res) => {
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
